30fed5d94b8193028ada5e5f7128e3c6ead90a49
BrightESTV <92432269+BrightESTV@users.noreply.github.com>
2021-10-21 22:23:02 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -102,30 +102,94 @@
 // 比较大小，我想写个函数，但是失败了
 //头文件必须引用对，否则全是错误
 #include <stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    int d = 0;
+//    scanf_s("%d %d %d", &a, &b, &c);
+//    if (a <= b)
+//    {
+//       int d = a;
+//        a = b;
+//        b = d;
+//    }
+//    if (a <= c)
+//    {
+//       int d = a;
+//        a = c;
+//        c = d;
+//    }
+//    if (b <= c)
+//    {
+//       int d = b;
+//        b = c;
+//        c = d;
+//    }
+//    printf("%d>%d>%d", a, b, c);
+//}
+// %取余数
+//辗转相除法
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf_s("%d %d",&a,&b);
+//	while (a % b)
+//	{
+//		c = a % b;
+//		b = a;
+//		a = c;
+//		printf("%d", c);
+//	}
+//
+//  
+//}
+//普通闰年：公历年份是4的倍数，且不是100的倍数的，为闰年（如2004年、2020年等就是闰年）。
+//世纪闰年：公历年份是整百数的，必须是400的倍数才是闰年（如1900年不是闰年，2000年是闰年）
+//试除法
+//sqrt 开方 需要引用<math.h>
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (((year%4 == 0) && (year % 100 != 0)) || (year%400 == 0))
+//			printf("%d\n",year);
+//		count++;
+//	}
+//
+//
+//	printf(" count =%d",count);
+//
+//	return 0;
+//}
+#include <math.h>
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    scanf_s("%d %d %d", &a, &b, &c);
-    if (a <= b)
-    {
-        d = a;
-        a = b;
-        b = d;
-    }
-    if (a <= c)
-    {
-        d = a;
-        a = c;
-        c = d;
-    }
-    if (b <= c)
-    {
-        d = b;
-        b = c;
-        c = d;
-    }
-    printf("%d>%d>%d", a, b, c);
+	int i = 0;
+	int count = 0;
+	for (i = 101; i <= 200; i+=2)
+	{
+		int j = 0;
+		for (j = 2; j<=sqrt(i);j++)
+			if (i%j == 0)
+ 			{  
+				break;
+			}
+		if (j>sqrt(i))
+			{
+				count++;
+				printf("%d\n",i);
+			}
+		
+	}
+	printf("\ncount = %d\n",count);
+
+	return 0;
 }
